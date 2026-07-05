321207f952430ef9ffc6c1d1007c95e53d566881
Thomas Farthing <monkeysmarts@gmail.com>
2026-07-05 16:57:23 -0700

    .

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ set ROOT=%cd%
 echo %ROOT%
 set PATH=%cd%\nasm;%cd%\strawberry-perl\perl\site\bin;%cd%\strawberry-perl\perl\bin;%cd%\strawberry-perl\c\bin;%PATH%
 cd openssl-3.2.1
-set CPPFLAGS=/MT
+set CPPFLAGS=/MT /Z7
 perl Configure no-shared
 nmake
 nmake test
